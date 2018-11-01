2a972af5f9757e4eb0bffbd7db7c8ba8b82f015b
EL-S <31227083+EL-S@users.noreply.github.com>
2018-11-01 22:07:18 +1100

    New Folder Creator
    Uses custom made osu file parser to extract information to create osu styled beatmap folders from a directory of ".osu" files that can then be used ingame.

--- osu_folder_creator.py
+++ osu_folder_creator.py
@@ -1,52 +1,76 @@
 import os
 import shutil
+import re
+import hashlib
+from osu_parser import parse
 
-path = 'test/'
-value = 0
+path = 'osu_files_testing/'
 
 for entry in os.scandir(path):
     file_path = entry.path
+    file_name = file_path[len(path):]
     if ".osu" in file_path:
-        no_folder = entry.path.split("/")[1] #no slashes allowed in file name
-        print(file_path)
         try:
-            first = no_folder.split("(")[0] #potential way
-        except:
-            first = ""
-        try:
-            second = no_folder.split("-")[0]+"-"+no_folder.split("-")[1].split("(")[0] #different potential way
-        except:
-            second = ""
-        try:
-            third = no_folder.split("(")[:-1][0] #another potential way
-        except:
-            third = ""
-        potential_folder_name = (first,second,third)
-        if len(first) > len(second): #yuckky
-            if len(first) > len(third):
-                value = 0
-            elif len(first) == len(third):
-                value = 0
-            else:
-                value = 2
-        elif len(first) == len(second):
-            if len(first) > len(third):
-                value = 0
-            else:
-                value = 2
-        elif len(second) > len(third):
-            value = 1
-        else:
-            value = 2
-        folder_name = potential_folder_name[value]
-        directory = (path+folder_name).strip()
-        try:
-            os.stat(directory)
-        except:
-            os.mkdir(directory)
-        try:
-            new_location = directory+"/"+no_folder
-            shutil.move(file_path, new_location)
-        except:
-            print("trailing zeros or something preventing correct folder naming")
-    
+            information,sections,hit_objects = parse(file_path)
+            try:
+                folder_name = str(information['BeatmapSetID'])+" "+str(information['Artist'])+" - "+str(information['Title'])
+                folder_name = re.sub('[\\\\/:*?"<>|]', '', folder_name).rstrip(".") #removes invalid folder characters and all trailing periods
+                folder_name = re.sub(r"\t", "", folder_name) #remove tabs
+                directory = (path+folder_name).strip()
+                try:
+                    os.stat(directory)
+                except:
+                    os.mkdir(directory)
+                try:
+                    new_location = (directory+"/"+file_name).strip()
+                    shutil.move(file_path, new_location)
+                except Exception as e:
+                    print("Error Officially Parsing:",file_path,e)
+            except: #it doesn't have a beatmapset id
+                try:
+                    unique_string = ""
+                    try:
+                        unique_string += information['Title']
+                    except:
+                        pass
+                    try:
+                        unique_string += information['TitleUnicode']
+                    except:
+                        pass
+                    try:
+                        unique_string += information['Artist']
+                    except:
+                        pass
+                    try:
+                        unique_string += information['ArtistUnicode']
+                    except:
+                        pass
+                    try:
+                        unique_string += information['AudioFilename']
+                    except:
+                        pass
+                    unique_string = str(unique_string).encode("utf-8")
+                    hasher = hashlib.sha1(unique_string)
+                    unique_hash = hasher.hexdigest()[0:5]
+                    artist = str(information['Artist']).lstrip(" ")
+                    title = str(information['Title']).lstrip(" ")
+                    folder_name = artist+" - "+title
+                    folder_name = re.sub('[\\\\/:*?"<>|]', '', folder_name).rstrip(".")+" ("+str(unique_hash)+")" #removes invalid folder characters and all trailing periods
+                    folder_name = re.sub(r"\t", "", folder_name) #remove tabs
+                    directory = (path+folder_name).strip()
+                    try:
+                        os.stat(directory)
+                    except:
+                        try:
+                            os.mkdir(directory)
+                        except Exception as e:
+                            print("Error Creating Directory:",file_path,e)
+                    try:
+                        new_location = (directory+"/"+file_name).strip()
+                        shutil.move(file_path, new_location)
+                    except Exception as e:
+                        print("Error Unofficially Parsing:",file_path,e,unique_string)
+                except Exception as e:
+                    print("Error Unofficially Parsing:",file_path,e,unique_string)
+        except Exception as e:
+            print("Error Parsing:",file_path,e)
