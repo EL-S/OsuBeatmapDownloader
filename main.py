750438f4f61cab81c7519c88d3f954c7b52da625
EL-S <31227083+EL-S@users.noreply.github.com>
2018-11-06 22:14:08 +1100

    Implemented Retry Function and Delay
    When the request fails, it waits and then retries the same request.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 from urllib.request import Request, urlopen
+import time
 
 directory = "osu_files/"
+#466973
+#815711
 start = 0
-end = 869742+1000
+end = 1100000+30000
 
 def check_file():
     global directory
@@ -12,23 +15,28 @@ def check_file():
     except:
         os.mkdir(directory)
 
+def make_request(i):
+    global directory
+    time.sleep(0.25)
+    url = "https://osu.ppy.sh/osu/" + str(i)
+
+    request = Request(url)
+
+    page = urlopen(request)
+    try:
+        file_name = page.headers['Content-Disposition'].split('"')[1]
+        full_path = directory + file_name
+        data = page.read().decode()
+        with open(full_path, "w", encoding="utf-8") as file:
+            file.write(data)
+        print("alive",url)
+    except Exception as e:
+        print("dead",url,e)
 check_file()
 
 for i in range(start,end+1):
     try:
-        url = "https://osu.ppy.sh/osu/" + str(i)
-
-        request = Request(url)
-
-        page = urlopen(request)
-        try:
-            file_name = page.headers['Content-Disposition'].split('"')[1]
-            full_path = directory + file_name
-            data = page.read().decode()
-            with open(full_path, "w", encoding="utf-8") as file:
-                file.write(data)
-            print("alive",url)
-        except Exception as e:
-            print("dead",url,e)
+        make_request(i)
     except Exception as e:
         print(e)
+        make_request(i)
